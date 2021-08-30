cf6e49e635b250c073b174b529fd5c2dfd09be97
tomo <tomoltk@gmail.com>
2021-08-30 11:34:41 +0700

    First build attempt

---
+++ precache-manifest.a4cea2fc236132b0cdb4ba784db71032.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63110acc33a9d707ee9233f245796c17",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b0a6e89914542e9cdce3",
+    "url": "./static/css/2.9cb0f446.chunk.css"
+  },
+  {
+    "revision": "800f1bdebd152f1ffb46",
+    "url": "./static/css/main.fd3e022a.chunk.css"
+  },
+  {
+    "revision": "b0a6e89914542e9cdce3",
+    "url": "./static/js/2.3ec34e2e.chunk.js"
+  },
+  {
+    "revision": "fd4df4f3388465cdce49097f55190b20",
+    "url": "./static/js/2.3ec34e2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "800f1bdebd152f1ffb46",
+    "url": "./static/js/main.b47ba9e9.chunk.js"
+  },
+  {
+    "revision": "028762adaf48b66839f6",
+    "url": "./static/js/runtime-main.08baa623.js"
+  }
+]);
